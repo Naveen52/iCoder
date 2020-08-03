efc315714ad212a85c05d871ea1830314aae24f7
Naveen52 <naveen.choudhary288@gmail.com>
2020-08-03 18:01:58 +0530

    all project

--- iCoder/blog/models.py
+++ iCoder/blog/models.py
@@ -1,11 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
 
 # Create your models here.
 class Post(models.Model):
-    id = models.AutoField(primary_key=True)
+    sno = models.AutoField(primary_key=True)
     title = models.CharField(max_length=30)
     content = models.TextField()
     author = models.CharField(max_length=12)
     slug = models.SlugField(max_length=78)
     timestamp = models.DateTimeField(blank=True)
+    def __str__(self):
+        return self.title
+    
 
+class blogComments(models.Model):
+    sno = models.AutoField(primary_key=True)
+    comment= models.TextField()
+    User = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
+    timestamp= models.DateTimeField(default=now)
--- iCoder/blog/views.py
+++ iCoder/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render,HttpResponse
-from.models import Post
+from django.shortcuts import render,HttpResponse, redirect
+from.models import Post,blogComments
+from django.contrib import messages
 # Create your views here.
 
 def blogHome(request):
@@ -10,7 +11,20 @@ def blogHome(request):
 
 def blogPost(request, slug):
    post = Post.objects.filter(slug=slug).first()
-
-   context = {'post':post}
+   comments = blogComments.objects.filter(post=post)
+ 
+   context = {'post':post, 'comments':comments}
    # return HttpResponse('this is blogPost')
    return render(request, 'blog/blogPost.html', context)
+
+def postComment(request):
+   if request.method == "POST":
+      comment =request.POST.get("comment")
+      user = request.user
+      postSno = request.POST.get("postSno")
+      post= Post.objects.get(sno=postSno)
+
+      comment = blogComments(comment=comment, User=user, post=post)
+      comment.save()
+      messages.success(request,"your comments has been successfully added")
+   return redirect(f"/blog/{post.slug}")
